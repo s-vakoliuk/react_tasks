2ab7ed135df7ebb0143ee637135ef6137096814a
s-vakoliuk <s.vakoliuk@gmail.com>
2022-01-18 20:23:59 +0200

    18 January 2022

---
+++ src/components/User/User.js
@@ -0,0 +1,12 @@
+import css from './User.module.css';
+
+const User = ({user}) => {
+    const {id, name, username, email} = user;
+    return (
+        <div className={css.wrap}>
+            <div>{id}) {name} -- {username} -- {email}</div>
+        </div>
+    );
+};
+
+export default User;
